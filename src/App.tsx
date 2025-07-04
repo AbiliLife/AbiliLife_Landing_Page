6f69b85dd342a2dc8cdbcd320575283d1c85c23f
Eli-Keli <muthokaelikeli@gmail.com>
2025-07-04 22:08:48 +0300

    FT:  Tailwind CSS Setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Welcome to AbiliLife</h1>
+      <h1 className='text-3xl font-bold underline'>
+        Welcome to AbiliLife
+      </h1>
       <p>Your journey to a better life starts here.</p>
       <button onClick={() => alert('Button clicked!')}>Click Me</button>
     </div>
