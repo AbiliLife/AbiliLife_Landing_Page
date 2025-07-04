04ca23fc831d0e897b5b3602ea2000250831adf0
Eli-Keli <muthokaelikeli@gmail.com>
2025-07-04 22:02:24 +0300

    FT: CREATING NEW VITE PROJECT SETUP

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to AbiliLife</h1>
+      <p>Your journey to a better life starts here.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </div>
+  )
+}
